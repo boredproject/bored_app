0bf7d11b046a53ba5e052eadeb12b62236984b3d
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-11 00:35:09 +0100

    feat:incremented new buttons for login and history
    +added the all rights reserved section on the bottom
    +added a different on selection color wich is #5E2BFF and text color on selection is #26E8A0

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import mainButton from './components/mainButton.vue'
 import footerWave from './components/footerWave.vue'
+import LogAndAcc from './components/logAndAcc.vue';
 </script>
 
 <template>
   <mainButton />
   <footerWave />
+  <LogAndAcc />
 </template>

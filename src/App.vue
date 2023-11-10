8c3d5d810d51d0eac92f5edff6efc5bdb6f9e4be
Victor Chabeau <victorchabeau03@gmail.com>
2023-11-10 19:54:59 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+<<<<<<< Updated upstream
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <HelloWorld />
+=======
+import Main from './components/Main.vue'
+</script>
+
+<template>
+  <Main />
+>>>>>>> Stashed changes
 </template>
---
+++ src/components/Main.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-center align-bottom flex-col text-center">
+        <p>All rights reserved ©</p>
+        <p>HETIC students 2023</p>
+    </div>
+</template>

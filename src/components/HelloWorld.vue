f3f83caf760e1acfd332ea66972fe014a7078932
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-10 17:44:00 +0100

    feat: Minor fixes on the landing page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="uppercase font-bold text-4xl">
-      Hello, im
-      <span class="bg-[#EE6498] p-1 hover:bg-white hover:text-[#ee6489] cursor-pointer">Bored</span>
+  <div class="uppercase font-bold align-middle text-center flex flex-col gap-5">
+    <div class="flex justify-center align-middle">
+        <p class="text-9xl bg-[#EE6498] p-5 transition-all duration-100 ease-in-out hover:bg-white hover:text-[#ee6489] cursor-pointer rounded-md max-w-max"
+        >Bored
+        </p>
+    </div>
+      
+    <div class="text-sm flex flex-col align-middle justify-center gap-5">
+      <div>
+        <p class="text-center text-2xl text-[#26E8A0]">HETIC</p>
+        <p class="text-center">Students</p>
+      </div>
+      <p class="text-center font-extralight">Samy , Icham , Wissem , Victor , Alexis , Maxime , Jean .</p>
+    </div>
   </div>
 </template>

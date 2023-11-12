46fb0f0f900a4d84cc89ea4a86692b3406060013
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-12 19:06:58 +0100

    feat:page buttons are made

--- app/selection/SelectionPage.tsx
+++ app/selection/SelectionPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navigation from '../homepage/Navigation/Navigation'
-import IdeaButton from './component/IdeaButton'
+import IdeaButton from './component/IdeaButtonSingle/IdeaButton'
+import ButtonSelection from './component/ButtonSelection/ButtonSelection'
 
 const SelectionPage = () => {
   return (
     <>
         <Navigation />
-        <IdeaButton />
+        <ButtonSelection />
 
     </>
   )

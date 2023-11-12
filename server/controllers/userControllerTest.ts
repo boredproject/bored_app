35acabdd1998f1b052365977f816c305de1fcd41
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-12 22:06:44 +0100

    fix:fixed folders incrementation
    -Created a server side folder so we cant split work between front and back end operators.
    -Created a mockup for backend folder structuration DO NOT CHANGE without approvement

---
+++ server/controllers/userControllerTest.ts
@@ -0,0 +1,17 @@
+// server/tests/controllers/userController.test.ts
+// import { expect } from 'chai';
+// import { describe, it } from 'mocha'; // Ces imports sont spécifiques à Mocha
+// import { UserController } from '../../controllers/userController';
+
+// describe('UserController', () => {
+//   it('should return user data', () => {
+//     const userController = new UserController();
+//     const userData = userController.getUserData();
+
+//     expect(userData).to.deep.equal({
+//       id: 1,
+//       username: 'john_doe',
+//       email: 'john.doe@example.com',
+//     });
+//   });
+// });

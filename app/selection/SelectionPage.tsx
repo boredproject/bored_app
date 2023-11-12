c8e61d7510e8117e1a8492058ecec8c233767828
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-12 19:07:43 +0100

    fix:added footer

--- app/selection/SelectionPage.tsx
+++ app/selection/SelectionPage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navigation from '../homepage/Navigation/Navigation'
 import IdeaButton from './component/IdeaButtonSingle/IdeaButton'
 import ButtonSelection from './component/ButtonSelection/ButtonSelection'
+import WaveFooter from '../homepage/Footer/WaveFooter'
 
 const SelectionPage = () => {
   return (
     <>
         <Navigation />
         <ButtonSelection />
+        <WaveFooter />
 
     </>
   )

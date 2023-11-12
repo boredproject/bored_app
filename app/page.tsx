9038efd6a73bc64fa098734481ee89cb3e11af2c
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-12 18:46:51 +0100

    feat:added button on the thing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import MainPage from "./MainPage/MainPage";
+import MainPage from "./homepage/MainPage";
 
 const Home = () => {
   return (
---
+++ app/selection/SelectionPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from '../homepage/Navigation/Navigation'
+import IdeaButton from './component/IdeaButton'
+
+const SelectionPage = () => {
+  return (
+    <>
+        <Navigation />
+        <IdeaButton />
+
+    </>
+  )
+}
+
+export default SelectionPage

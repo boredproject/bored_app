c736af2b710f64551b8d26aa89379f8bd2c5f8b6
Wissem95 <wissemkarboub@gmail.com>
2023-11-15 02:20:05 +0100

    feat: added begin of dropdownmenu

--- app/homepage/HomePage.tsx
+++ app/homepage/HomePage.tsx
@@ -2,6 +2,7 @@ import WaveFooter from "./Footer/WaveFooter";
 import Navigation from "./Navigation/Navigation";
 import CtaMainpage from "./CTA/CtaMainpage";
 import SoundEffects from "../SFX/SoundEffects";
+import DropdownMenuProfile from "@/app/homepage/Navigation/Dropdown/DropdownMenu";
 import Loader from "./Loader/Loader";
 
 const HomePage = () => {
@@ -10,6 +11,7 @@ const HomePage = () => {
         <WaveFooter />
         <Loader />
         <Navigation />
+        <DropdownMenuProfile />
         <CtaMainpage />
         <SoundEffects />
     </div>

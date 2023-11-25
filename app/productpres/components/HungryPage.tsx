53eec50c66f62064a0f9d579d735ec9ac846ee01
Alexis Janus <janusalexis1@gmail.com>
2023-11-25 21:37:53 +0100

    HungryPage css

--- app/productpres/components/HungryPage.tsx
+++ app/productpres/components/HungryPage.tsx
@@ -1,12 +1,28 @@
+// HungryPage.tsx
 import React from 'react';
-import Link from "next/link";
+import Link from 'next/link';
+import IdeaButton from '@/app/selection/component/ButtonSelection/IdeaButtonSingle/IdeaButton';
+import styles from './HungryPage.module.css';
+import WaveFooter from "../../homepage/Footer/WaveFooter";
+import Navigation from "../../homepage/Navigation/Navigation";
 
 const HungryPage = () => {
-    return (
-        <div>
-            <h1>HELLO</h1>
+  return (
+    <div>
+        <Navigation />
+        <div className={styles.container}>
+            <IdeaButton type="svg1" textspin="spinnersvg1" />
+            <div className={styles.rightPanel}>
+                <div className={styles.innerPanel}>Berliner Kebab</div>
+                <div className={styles.innerPanel}>Berliner Kebab</div>
+                <div className={styles.innerPanel}>Berliner Kebab</div>
+                <div className={styles.innerPanel}>Berliner Kebab</div>
+                <div className={styles.innerPanel}>Berliner Kebab</div>
+            </div>
         </div>
-    );
+        <WaveFooter />
+    </div>
+  );
 };
 
-export default HungryPage
+export default HungryPage;

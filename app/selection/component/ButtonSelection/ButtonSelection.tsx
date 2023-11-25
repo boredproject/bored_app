9c14e44d391e3b39cb277f602051b5ccb35fee3f
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-25 19:21:23 +0100

    fix:link

--- app/selection/component/ButtonSelection/ButtonSelection.tsx
+++ app/selection/component/ButtonSelection/ButtonSelection.tsx
@@ -9,12 +9,15 @@ const ButtonSelection = () => {
   return (
     <div className='flex flex-col w-screen'>
       <div className='lg:flex lg:flex-row lg:justify-evenly hidden flex-col lg:gap-44'>
-        <Link href="/HungryPage/">
+        <Link href="/productpres">
           <IdeaButton type="svg1" textspin='spinnersvg1' /> {/* Vous pouvez changer le type ici */}
         </Link>
-        
-        <IdeaButton type="svg2" textspin='spinnersvg2'/> {/* Vous pouvez changer le type ici */}
-        <IdeaButton type="svg3" textspin='spinnersvg3'/> {/* Vous pouvez changer le type ici */}
+        <Link href="/productpres">
+          <IdeaButton type="svg2" textspin='spinnersvg2'/> {/* Vous pouvez changer le type ici */}
+        </Link>
+        <Link href="/productpres">
+          <IdeaButton type="svg3" textspin='spinnersvg2'/> {/* Vous pouvez changer le type ici */}
+        </Link>
       </div>
       <div className='lg:hidden flex h-screen w-screen flex-col justify-center items-center'>
         <div className='flex md:flex-row md:gap-64 flex-col justify-between gap-52'>

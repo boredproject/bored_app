a5d4cb465cf86fbc1b765467b3eefc7b9a238249
Victor Chabeau <victorchabeau03@gmail.com>
2023-11-26 15:59:58 +0100

    fix: modal desktop display

--- app/productpres/components/modal-project.tsx
+++ app/productpres/components/modal-project.tsx
@@ -12,23 +12,22 @@ import React from 'react';
 const Modal = () => {
     return (
         <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
-            {/* Mobile Display */}
-            <div className="lg:hidden flex bg-[#181818] p-6 h-3/4 w-3/4 border-solid border-[#5E2BFF] border-4 rounded text-white flex-col overflow-auto">
+            {/* Mobile Display & Desktop Display */}
+            <div className="flex bg-[#181818] p-6 h-3/4 lg:w-2/3 w-3/4 border-solid border-[#5E2BFF] border-4 rounded text-white flex-col lg:flex-row lg:flex-wrap overflow-auto lg:justify-around">
                 {/* image du lieu */}
-                <img alt="place-preview" className='border-solid border-[#5E2BFF] border-4 rounded w-full h-1/4' />
-                <h1 className='font-bold text-3xl'>Nom du lieux</h1>
-                <h2 className='text-xl'>19h45 - 23h00</h2>
-                <p>adresse complète</p>
+                <img alt="place-preview" className='border-solid border-[#5E2BFF] border-4 rounded w-full h-1/4 lg:h-auto lg:w-1/3 lg:mb-5' />
+                <div className='lg:flex lg:flex-col'>
+                    <h1 className='font-bold text-3xl lg:text-5xl'>Nom du lieux</h1>
+                    <h2 className='text-xl'>19h45 - 23h00</h2>
+                    <p className='mb-5'>adresse complète</p>
+                </div>
                 {/* filtres API */}
                 <div className='border-solid border-[#5E2BFF] border-4 rounded h-6 w-full'></div>
-                {/* API Google Maps */}
-                {/* <Map className='border-solid border-[#5E2BFF] border-4 rounded h-auto w-auto'>
-
-                </Map> */}
-                <p className='order'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque doloremque blanditiis ipsam a in accusamus minus ut! Reiciendis doloremque nostrum qui nobis voluptate ea temporibus fugit vero eum architecto. Velit.</p>
+                    <p className='lg:w-3/6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque doloremque blanditiis ipsam a in accusamus minus ut! Reiciendis doloremque nostrum qui nobis voluptate ea temporibus fugit vero eum architecto. Velit.</p>
+                    {/* API Google Maps */}
+                    {/* <Map className='border-solid border-[#5E2BFF] border-4 rounded h-auto w-auto'>
+                    </Map> */}
             </div>
-            {/* Desktop Display */} 
-
         </div>
     );
 };
--- app/productpres/page.tsx
+++ app/productpres/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import HungryPage from './components/HungryPage';
-import Modal from './components/modal-project';
 
 const ProductPres = () => {
     return (
         <>
             <HungryPage />
-            <Modal />
         </>
     );
 };

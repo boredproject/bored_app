80c56bae1d2843269f592c4d645a3788aa357144
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-27 15:39:49 +0100

    fix:init

---
+++ app/productpage/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import ProductPage from "./ProductPage";
+
+const ProdPage = () => {
+  return <div>
+    <ProductPage />
+  </div>;
+};
+
+export default ProdPage;

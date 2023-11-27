da3146f60506078c7cd387a23802802a1b4e59f7
Samy Hamlat <samy.hamlat@manutan.fr>
2023-11-27 16:10:30 +0100

    feat : start of product page

--- app/productpage/ProductPage.tsx
+++ app/productpage/ProductPage.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import IdeaButton from "../selection/component/ButtonSelection/IdeaButtonSingle/IdeaButton";
 
 const ProductPage = () => {
-  return (
-    <div className="flex justify-center align-middle h-screen">
-      <div className="flex justify-center align-middle h-screen flex">
-        <IdeaButton type="svg1" textspin="spinnersvg1" />
-        <div>Tableau</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="">
+            <div className="flex justify-center items-center">
+                <IdeaButton type="svg1" textspin="spinnersvg1"/>
+                <div>
+                    <div>Tableau</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ProductPage;

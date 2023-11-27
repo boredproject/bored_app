47412771b20c729eb844cbc2db143fc61222bf9f
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-27 15:44:44 +0100

    fix/table
    ble

--- app/productpage/ProductPage.tsx
+++ app/productpage/ProductPage.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import IdeaButton from "../selection/component/ButtonSelection/IdeaButtonSingle/IdeaButton";
 
 const ProductPage = () => {
   return (
-    <div>ProductPage</div>
-  )
-}
+    <div className="flex justify-center align-middle h-screen">
+      <div className="flex justify-center align-middle h-screen flex">
+        <IdeaButton type="svg1" textspin="spinnersvg1" />
+        <div>Tableau</div>
+      </div>
+    </div>
+  );
+};
 
-export default ProductPage
+export default ProductPage;
--- app/productpage/page.tsx
+++ app/productpage/page.tsx
@@ -2,10 +2,14 @@
 
 import React from "react";
 import ProductPage from "./ProductPage";
+import Navigation from "../homepage/Navigation/Navigation";
+import WaveFooter from "../homepage/Footer/WaveFooter";
 
 const ProdPage = () => {
   return <div>
+    <Navigation />
     <ProductPage />
+    <WaveFooter />
   </div>;
 };
 

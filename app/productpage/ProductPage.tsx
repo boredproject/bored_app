0777260d36728220921c34a08ba2066c823b742a
Samy Hamlat <samy.hamlat@manutan.fr>
2023-11-27 16:39:52 +0100

    feat : table created, has to make the mobile version

--- app/productpage/ProductPage.tsx
+++ app/productpage/ProductPage.tsx
@@ -3,11 +3,22 @@ import IdeaButton from "../selection/component/ButtonSelection/IdeaButtonSingle/
 
 const ProductPage = () => {
     return (
-        <div className="">
-            <div className="flex justify-center items-center">
-                <IdeaButton type="svg1" textspin="spinnersvg1"/>
+        <div className="flex justify-between">
+            <div className="flex justify-center items-center px-5">
                 <div>
-                    <div>Tableau</div>
+                <IdeaButton type="svg1" textspin="spinnersvg1"/>
+                </div>
+                <div className={"border-2 border-[#5E2BFF] p-2 rounded-lg"}>
+                    <table className={"border-[#5E2BFF]  border-separate border-spacing-2 rounded-lg"}>
+                        <tbody className={"border-spacing-2 rounded-lg"}>
+                        <tr>
+                            <td className={"border-2 border-[#5E2BFF] rounded p-2"}>3fdjsfbdsk zehozelgh svgdshigj dlsgdsoi j3fdjsfbdsk zehozelgh svgdshigj dlsgdsoi jf3fdjsfbdsk zehozelgh svgdshigj dlsgdsoi jf3fdjsfbdsk zehozelgh svgdshigj dlsgdsoi jf3fdjsfbdsk zehozelgh svgdshigj dlsgdsoi jff</td>
+                        </tr>
+                        <tr>
+                            <td className={"border-2 border-[#5E2BFF] rounded p-2"}>3</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>

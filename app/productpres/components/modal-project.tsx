467171b48e852745f78cd7a8a36e78c9a31e629d
Victor Chabeau <victorchabeau03@gmail.com>
2023-11-28 01:41:15 +0100

    fix: Modal mobile and desktop display done

--- app/productpres/components/modal-project.tsx
+++ app/productpres/components/modal-project.tsx
@@ -1,24 +1,17 @@
 "use client";
 
 import React from 'react';
-// import {
-//     APIProvider,
-//     Map,
-//     AdvancedMarker,
-//     Pin,
-//     InfoWindow,
-// } from "@vis.gl/react-google-maps";
 
 const Modal = () => {
     return (
         <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
             {/* Mobile Display & Desktop Display */}
-            <div className="flex bg-[#181818] p-6 h-3/4 lg:w-2/3 w-3/4 border-solid border-[#5E2BFF] border-4 rounded text-white flex-col lg:flex-row lg:flex-wrap overflow-auto lg:justify-around">
+            <div className="flex bg-[#181818] p-6 h-3/4 lg:w-2/3 w-3/4 border-solid border-[#5E2BFF] border-4 rounded text-white flex-col lg:flex-row lg:flex-wrap overflow-auto lg:justify-start">
                 {/* image du lieu */}
                 <img alt="place-preview" className='border-solid border-[#5E2BFF] border-4 rounded w-full h-1/4 lg:h-auto lg:w-1/3 lg:mb-5' />
-                <div className='lg:flex lg:flex-col'>
+                <div className='lg:flex lg:flex-col lg:ml-5'>
                     <h1 className='font-bold text-3xl lg:text-5xl'>Nom du lieux</h1>
-                    <h2 className='text-xl'>19h45 - 23h00</h2>
+                    <h2 className='text-xl'>horaires ouverture - fermeture</h2>
                     <p className='mb-5'>adresse complète</p>
                 </div>
                 {/* filtres API */}

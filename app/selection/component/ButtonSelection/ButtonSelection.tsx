a9c7013161b7dd88507fe3fdf718e28b9e248481
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-11-28 04:49:09 +0100

    fix:added links to the product page

--- app/selection/component/ButtonSelection/ButtonSelection.tsx
+++ app/selection/component/ButtonSelection/ButtonSelection.tsx
@@ -3,15 +3,22 @@
 
 import React from 'react';
 import IdeaButton from './IdeaButtonSingle/IdeaButton';
+import Link from 'next/link';
 
 
 const ButtonSelection = () => {
   return (
     <div className='flex flex-col w-screen'>
       <div className='lg:flex lg:flex-row lg:justify-evenly hidden flex-col lg:gap-44'>
-      <IdeaButton type="svg1" textspin='spinnersvg1' /> {/* Vous pouvez changer le type ici */}
-      <IdeaButton type="svg2" textspin='spinnersvg2'/> {/* Vous pouvez changer le type ici */}
-      <IdeaButton type="svg3" textspin='spinnersvg3'/> {/* Vous pouvez changer le type ici */}
+        <Link href='/productpage'>
+        <IdeaButton type="svg1" textspin='spinnersvg1' /> {/* Vous pouvez changer le type ici */}
+        </Link>
+        <Link href='/productpage'>
+        <IdeaButton type="svg2" textspin='spinnersvg2' /> {/* Vous pouvez changer le type ici */}
+        </Link>
+        <Link href='/productpage'>
+        <IdeaButton type="svg3" textspin='spinnersvg3' /> {/* Vous pouvez changer le type ici */}
+        </Link>
       </div>
       <div className='lg:hidden flex h-screen w-screen flex-col justify-center items-center'>
         <div className='flex md:flex-row md:gap-64 flex-col justify-between gap-52'>
